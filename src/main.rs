5ffe48f2d3f297b499829746e32b5d2dcf1780ad
Sander Hautvast <shautvast@gmail.com>
2022-01-26 18:52:22 +0100

    list on the heap - compiling

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ fn main() {
     
 }
 
-struct Node {
-    elem: i32,
-    next: List,
+pub struct List {
+    head: Link
 }
 
-pub enum List {
+enum Link{
     Empty,
     More(Box<Node>),
 }
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
